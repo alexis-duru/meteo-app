23755f008eb45472e989a5f322423566ef52470b
Alexis Duru <74984922+alexis-duru@users.noreply.github.com>
2023-09-15 16:45:38 +0200

    [addon] package manager + services file

---
+++ services/Api.tsx
@@ -0,0 +1,25 @@
+const getWeather = async (city: string) => {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=7425272eb22af607e6aaf2c4134b945e`,
+    );
+
+    if (!response.ok) {
+      throw new Error(
+        'Erreur lors de la récupération des données météorologiques.',
+      );
+    }
+
+    const data = await response.json();
+    console.log('Données météorologiques :', data);
+    return data;
+  } catch (error) {
+    console.error(
+      'Erreur lors de la récupération des données météorologiques :',
+      error,
+    );
+    throw error;
+  }
+};
+
+export default getWeather;
