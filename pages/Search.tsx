a671322b0de2c0f07ecdbfc9aac5326bff30f72e
Alexis Duru <74984922+alexis-duru@users.noreply.github.com>
2023-09-17 20:27:40 +0200

    [addon] création du composant de recherche

--- pages/Search.tsx
+++ pages/Search.tsx
@@ -1,13 +1,34 @@
+import {TextInput} from '@react-native-material/core';
 import {useNavigation} from '@react-navigation/native';
+import {useState} from 'react';
 import {Button, Text, View} from 'react-native';
+import getWeather from '../services/Api';
 
 const Search = () => {
   const {navigate}: any = useNavigation();
 
+  const [city, setCity] = useState('');
+
+  const handleSearch = async () => {
+    try {
+      const data = await getWeather(city);
+      // Mettez à jour l'état des données météorologiques ici
+      console.log('Données météorologiques :', data);
+      navigate('RESULTS', {weatherData: data});
+    } catch (error) {
+      console.error('Erreur lors de la recherche de météo :', error);
+    }
+  };
+
   return (
     <>
       <View>
-        <Text>Search</Text>
+        <TextInput
+          placeholder="Entrez le nom de la ville"
+          value={city}
+          onChangeText={setCity}
+        />
+        <Button title="Rechercher" onPress={handleSearch} />
       </View>
     </>
   );
