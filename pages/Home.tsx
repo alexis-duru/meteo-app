12cfd3c8eb76de5e9794f39396fb7d6ba6e84c6a
Alexis Duru <74984922+alexis-duru@users.noreply.github.com>
2023-09-17 20:27:20 +0200

    [addon] affichage de la météo de bordeaux sur la home

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -6,12 +6,35 @@ import {
   View,
   StyleSheet,
   Button,
+  Image,
 } from 'react-native';
 import Layout from '../components/Layout';
 import {useNavigation} from '@react-navigation/native';
+import getWeather from '../services/Api'; // Assurez-vous que le chemin d'importation est correct
+import React, {useState, useEffect} from 'react';
 
 const Homepage = () => {
+  const [weatherData, setWeatherData] = useState<any>(null);
+
   const {navigate}: any = useNavigation();
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      try {
+        const data = await getWeather('Bordeaux'); // Remplacez par la ville de Bordeaux si nécessaire
+        console.log('Données météorologiques :', data);
+        setWeatherData(data);
+      } catch (error) {
+        console.error(
+          'Erreur lors de la récupération des données météorologiques :',
+          error,
+        );
+      }
+    };
+
+    fetchWeatherData();
+  }, []);
+
   return (
     <>
       <Layout>
@@ -19,10 +42,25 @@ const Homepage = () => {
           <StatusBar />
           <ScrollView contentInsetAdjustmentBehavior="automatic">
             <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Homepage</Text>
-              <Text style={styles.sectionDescription}>
-                This is the homepage.
-              </Text>
+              <Text style={styles.sectionTitle}>Home</Text>
+              {weatherData ? (
+                <>
+                  <Text>Ville: {weatherData.city.name}</Text>
+                  <Text>Date: {weatherData.list[0].dt_txt}</Text>
+                  <Text>Température: {weatherData.list[0].main.temp} °C</Text>
+                  <Text>
+                    Météo: {weatherData.list[0].weather[0].description}
+                  </Text>
+                  <Image
+                    source={{
+                      uri: `https://openweathermap.org/img/w/${weatherData.list[0].weather[0].icon}.png`,
+                    }}
+                    style={{width: 50, height: 50}}
+                  />
+                </>
+              ) : (
+                <Text>Chargement des données météorologiques...</Text>
+              )}
             </View>
           </ScrollView>
         </SafeAreaView>
@@ -35,7 +73,6 @@ const styles = StyleSheet.create({
   sectionContainer: {
     paddingHorizontal: 24,
     height: 500,
-    backgroundColor: 'lightblue',
   },
   sectionTitle: {
     fontSize: 24,
