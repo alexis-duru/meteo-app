8c7af165087775df66424fb5aa431dd40640abd9
Alexis Duru <74984922+alexis-duru@users.noreply.github.com>
2023-09-17 20:27:31 +0200

    [addon] création du composant de résultats

--- pages/Results.tsx
+++ pages/Results.tsx
@@ -1,13 +1,31 @@
-import {Text, View} from 'react-native';
+import React from 'react';
+import {View, Text, Image, StyleSheet} from 'react-native';
 
-const Results = () => {
+const Results = ({route}: any) => {
+  const {weatherData} = route.params;
+
   return (
-    <>
-      <View>
-        <Text>Results</Text>
-      </View>
-    </>
+    <View style={styles.container}>
+      <Text>Ville: {weatherData.city.name}</Text>
+      <Text>Date: {weatherData.list[0].dt_txt}</Text>
+      <Text>Température: {weatherData.list[0].main.temp} °C</Text>
+      <Text>Météo: {weatherData.list[0].weather[0].description}</Text>
+      <Image
+        source={{
+          uri: `https://openweathermap.org/img/w/${weatherData.list[0].weather[0].icon}.png`,
+        }}
+        style={{width: 50, height: 50}}
+      />
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default Results;
